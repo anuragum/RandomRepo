30b82b00c05ecd18004d22efcd57bc56b1edd6fb
anuragumarkhede <anuragumarkhede1995@gmail.com>
2024-04-15 16:19:48 +0530

    Demo2.js added

---
+++ Demo2.js
@@ -0,0 +1,24 @@
+const names = [
+    {
+        id: 1,
+        name: "anurag",
+        age: 28,
+        isDeveloper: true
+    },
+    {
+        id: 2,
+        name: "sanjay",
+        age: 54,
+        isDeveloper: false
+    },
+    {
+        id: 3,
+        name: "manisha",
+        age: 50,
+        isDeveloper: false
+    }
+]
+
+names.map(name => {
+    console.log(name);
+})
